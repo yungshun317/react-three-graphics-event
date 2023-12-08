9d5c02ee384a01f4de75e7ac1816a8f577feb8f8
yungshun317 <seraph317@gmail.com>
2023-12-08 20:39:04 +0800

    [FEAT] Use the Leva GUI component for the React Three Fiber application

--- src/Scene.js
+++ src/Scene.js
@@ -1,6 +1,31 @@
 import { OrbitControls } from "@react-three/drei";
+import {button, useControls} from "leva";
 
 const Scene = () => {
+    const { position, color, wireframe, scale } = useControls("cube", {
+        position: {
+            value: {
+                x: 0,
+                y: 0,
+                z: 0
+            },
+            min: -10,
+            max: 10,
+            step: 0.01
+        },
+        color: "#ffffff",
+        wireframe: false,
+        click: button(() => {
+            console.log("clicked");
+        }),
+        scale: { options: [1, 2, 3] }
+    })
+    console.log(scale);
+
+    const sphereTweak = useControls("sphere", {
+        xRotation:0
+    })
+
     return (
         <>
             <OrbitControls/>
@@ -8,9 +33,9 @@ const Scene = () => {
             <ambientLight/>
             <directionalLight position={[0, 2, 4]}/>
 
-            <mesh>
+            <mesh position={[position.x, position.y, position.z]} scale={scale}>
                 <boxGeometry />
-                <meshStandardMaterial />
+                <meshStandardMaterial color={color} wireframe={wireframe} />
             </mesh>
         </>
     );
